5c33bf5809914e623d9b53d4e4472aa649d77c8b
sathviknori <sathvik.nori@gmail.com>
2022-04-03 14:49:07 -0700

    new work

--- server.ts
+++ server.ts
@@ -1,2 +1,44 @@
 //This is our server for Marabu
-//hih
+
+import { Socket, createServer, connect } from "net" 
+import * as stringify from 'canonical-json'
+
+
+
+
+const port = 18018
+const Net = require('net');
+
+
+class Server {
+    constructor() {
+        const newServ = net.createServer();
+        newServ.listen(port);
+       
+        console.log(`Server listening for requests on socket localhost:${port}`);
+        newServ.on('connection', (socket: Socket) => {
+			new Client(socket.remoteAddress, socket.remotePort, socket)
+		})
+
+    }
+}
+
+class Client {
+
+    private port: number
+    private host: string
+    private socket: Socket
+
+
+    constructor(host: string, port: number, socket?: socket) {
+        this.host = host;
+        this.port = port;
+
+        this.socket = socket;
+    }
+
+}
+
+
+
+
