4076079778f448c5b380f47c0aa394da1f2e4235
BarWeiner1 <barweiner03@gmail.com>
2022-04-05 13:07:07 -0700

    Changed structure

--- main.ts
+++ main.ts
@@ -10,7 +10,7 @@ const port = 18018;
 
 // Create a new TCP server.
 const server = new Net.Server()
-
+    
 
 
 server.listen(port, function() {
@@ -25,36 +25,9 @@ server.on('connection', function(socket : Socket) {
     // Now that a TCP connection has been established, the server can send data to
     // the client by writing to its socket.
     socket.write('Hello, client.');
-    console.log(`${socket}`);
-
+   // console.log(`${socket}`);
+    
+    console.log(`${socket.remoteAddress.toString()}`)
     new peer(socket.remoteAddress, socket.remotePort, socket)
     
-});
-
-
-//Create a new client
-const client = new Net.Socket();
-
-client.connect({ port: port, host: host }), function() {
-    // If there is no error, the server has accepted the request and created a new 
-    // socket dedicated to us.
-    console.log('TCP connection established with the server.');
-
-    // The client can now send data to the server by writing to its socket.
-    client.write('Hello, server.');
-};
-
-// The client can also receive data from the server by reading from its socket.
-client.on('data', function(address: string, port: number) {
-    console.log(`Data received from the server: ${address.toString()}.`);
-    
-    // Request an end to the connection after the data has been received.
-    client.end();
-});
-
-client.on('end', function() {
-    console.log('Requested an end to the TCP connection');
-});
-
-//Go to peer class to communicate through socket
-
+})
--- peer.ts
+++ peer.ts
@@ -1,5 +1,6 @@
 import {Socket, connect} from "net"
-
+const Net = require('net');
+const port = 18018;
 
 export default class peer{
     private socket: Socket
@@ -41,5 +42,30 @@ private fillBuffer(buffer: Buffer) {
 
 
 }
+
+
 }
+
+const client = new Net.Socket();
+client.connect({ port: port, host: host }), function() {
+    // If there is no error, the server has accepted the request and created a new 
+    // socket dedicated to us.
+    console.log('TCP connection established with the server.');
+
+    // The client can now send data to the server by writing to its socket.
+    client.write('Hello, server.');
+};
+
+// The client can also receive data from the server by reading from its socket.
+client.on('data', function(address: string, port: number) {
+    console.log(`Data received from the server: ${address.toString()}.`);
+    
+    // Request an end to the connection after the data has been received.
+    client.end();
+});
+
+client.on('end', function() {
+    console.log('Requested an end to the TCP connection');
+});
+
 
